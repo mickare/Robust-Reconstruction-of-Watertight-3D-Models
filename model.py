fc20ce62d41c196a6762fafd24dc435dfe358de1
mickare <info@mickare.de>
2020-12-09 22:08:50 +0100

    add dragon render

--- model.py
+++ model.py
@@ -1,54 +1,96 @@
 import os
+from dataclasses import dataclass
+from typing import Optional, List, Sequence, Iterator
+
+import numpy as np
+import plyfile
+import plotly.graph_objects as go
+import tqdm
+
+from mathlib import quaternion_rotation_matrix
 
 
-class Model:
+class Transform:
+    translation: np.ndarray
+    rotation: np.ndarray
+
+    def __init__(self, translation: np.ndarray, rotation: np.ndarray):
+        assert translation.shape == (3,)
+        assert rotation.shape == (3, 3)
+        self.translation = translation
+        self.rotation = rotation
+
+    @classmethod
+    def from_quat(cls, transl, rot: np.ndarray):
+        return cls(transl, quaternion_rotation_matrix(rot))
+
+    @classmethod
+    def read(cls, data: Sequence[float]):
+        assert len(data) == 7
+        d = [float(d) for d in data]
+        return cls.from_quat(np.array(d[:3]), np.array(d[3:]))
+
+    def apply(self, points: np.ndarray):
+        assert points.shape[1] == 3
+        return np.dot((points + self.translation), self.rotation)
+
+
+class Scan:
+    _data: Optional[plyfile.PlyData] = None
+
+    def __init__(self, file: str, trans: Transform):
+        self.file = file
+        self.trans = trans
+
+    def get(self) -> plyfile.PlyData:
+        if self._data is None:
+            self._data = plyfile.PlyData.read(self.file)
+        return self._data
+
+    def points(self) -> np.ndarray:
+        p = self.get()["vertex"]
+        pts = np.transpose((p["x"], p["y"], p["z"]))
+        return self.trans.apply(pts)
+
+    def scatter(self, transf: Optional[Transform] = None, **kwargs):
+        pts = self.points()
+        if transf is not None:
+            pts = transf.apply(pts)
+        # Flip Y/Z
+        x, z, y = pts.T
+        return go.Scatter3d(x=x, y=y, z=z, **kwargs)
+
+
+@dataclass
+class ScanFolder:
+    transform: Transform
+    scans: List[Scan]
 
     @classmethod
     def load_ply_conf(cls, path: str):
         assert os.path.isfile(path)
 
+        base = os.path.dirname(path)
+
         with open(path, mode='tr') as fp:
             conf = fp.readlines()
+
+        transform: Optional[Transform] = None
+        scans: List[Scan] = []
 
         for line in conf:
             if line.startswith("camera"):
                 pass
             elif line.startswith("mesh"):
-                pass
+                l = line.split(" ")
+                transform = Transform.read(l[2:])
+
             elif line.startswith("bmesh"):
                 l = line.split(" ")
-                file = l[1]
+                scans.append(Scan(os.path.join(base, l[1]), Transform.read(l[2:])))
+        return cls(transform, scans)
 
+    def iter_points(self) -> Iterator[np.ndarray]:
+        for scan in self.scans:
+            yield scan.points()
 
-import plyfile
-import plotly.graph_objects as go
-
-data = plyfile.PlyData.read('models/dragon_stand/dragonStandRight_192.ply')
-
-vertex = data["vertex"]
-x = vertex.data["x"]
-y = vertex.data["y"]
-z = vertex.data["z"]
-
-camera = dict(
-    up=dict(x=0, y=1, z=0)
-)
-fig = go.Figure(
-    data=[
-        go.Scatter3d(x=x, y=z, z=y,
-                     mode='markers', marker=dict(color='red', size=1, ))
-    ]
-)
-fig.update_layout(
-    yaxis=dict(scaleanchor="x", scaleratio=1),
-    scene=dict(
-        aspectmode='data',
-        xaxis_title='X',
-        yaxis_title='Y',  # Flip Y/Z
-        zaxis_title='Z',  # Flip Y/Z
-        camera=camera,
-        dragmode='turntable'
-    ),
-    scene_camera=camera
-)
-fig.show()
---
+++ plot_dragon.py
@@ -0,0 +1,29 @@
+import plotly.graph_objects as go
+import tqdm
+
+from model import ScanFolder
+
+scans = ScanFolder.load_ply_conf("models/dragon_stand/dragonStandRight.conf")
+
+camera = dict(
+    up=dict(x=0, y=1, z=0)
+)
+fig = go.Figure(
+    data=[
+        s.scatter(transf=scans.transform, mode='markers', marker=dict(size=0.5))
+        for s in tqdm.tqdm(scans.scans, desc="Load files")
+    ]
+)
+fig.update_layout(
+    yaxis=dict(scaleanchor="x", scaleratio=1),
+    scene=dict(
+        aspectmode='data',
+        xaxis_title='X',
+        yaxis_title='Z',  # Flip Y/Z
+        zaxis_title='Y',  # Flip Y/Z
+        camera=camera,
+        dragmode='turntable'
+    ),
+    scene_camera=camera
+)
+fig.show()
