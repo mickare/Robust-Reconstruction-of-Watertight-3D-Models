1de889aa547c9d3902cc6a6885031127926ec76c
mickare <info@mickare.de>
2020-12-21 21:43:41 +0100

    improve dilate() operator

--- operators/dilate_operator.py
+++ operators/dilate_operator.py
@@ -8,28 +8,33 @@ from data.chunks import ChunkGrid, Chunk, ChunkIndex, ChunkFace
 from mathlib import Vec3i
 
 
-def dilate(image: ChunkGrid[bool], mask: ChunkGrid[bool] = None) -> ChunkGrid[bool]:
+def dilate(image: ChunkGrid[bool], mask: ChunkGrid[bool] = None, steps=1) -> ChunkGrid[bool]:
     if mask is None:
         mask = ChunkGrid(image.chunk_size, dtype=bool, fill_value=True)
     else:
         mask = mask.astype(bool)
 
     result = image.copy()
+    for step in range(steps):
+        # Dilate inner chunk
+        for r in result.chunks:
+            tmp = ndimage.binary_dilation(
+                r.to_array(),
+                mask=mask.ensure_chunk_at_index(r.index, insert=False).to_array()
+            )
+            r.set_array(tmp)
 
-    # Dilate inner chunk
-    for r in result.chunks:
-        tmp = ndimage.binary_dilation(r.to_array(), mask=mask.ensure_chunk_at_index(r.index, insert=False).to_array())
-        r.set_array(tmp)
-
-    # Dilate chunk overflow
-    for index in list(result.chunks.keys()):
-        for f, n in result.iter_neighbors_indicies(index):
-            r_n = result.chunks.get(n, None)
-            if r_n is not None:
+        # Dilate chunk overflow
+        for index in list(result.chunks.keys()):
+            for f, n in result.iter_neighbors_indicies(index):
+                # Only insert if it is filled by overflow
+                r_n = result.ensure_chunk_at_index(n, insert=False)
                 m = mask.ensure_chunk_at_index(index, insert=False).to_array()
                 img = image.ensure_chunk_at_index(index, insert=False).to_array()
                 s0 = f.slice()
                 s1 = f.flip().slice()
                 r_n[s1] |= m[s0] & img[s0]
                 r_n.cleanup_memory()
+                if r_n.any():
+                    result.chunks.insert(n, r_n)
     return result
