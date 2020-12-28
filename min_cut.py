d53577743843ed4602fc6278348a4b42e4cc5dcf
diokithr <corianderaberdeen@yahoo.de>
2020-12-29 00:01:04 +0100

    Fixed some min cut bugs, outputs only one segment though.

--- min_cut.py
+++ min_cut.py
@@ -3,6 +3,9 @@ from model.model_pts import PtsModelLoader
 from make_crust import get_crust, get_diffusion, scale_model
 import numpy as np
 
+from render_cloud import CloudRender
+from render_voxel import VoxelRender
+
 data = PtsModelLoader().load("models/bunny/bunnyData.pts")
 # data = PlyModelLoader().load("models/dragon_stand/dragonStandRight.conf")
 # data = MeshModelLoader(samples=30000, noise=0.1).load("models/cat/cat_reference.obj")
@@ -20,7 +23,7 @@ model, model_offset, model_scale = scale_model(data, resolution=64)
 crust = get_crust(chunk_size, max_steps, num_revert_steps, model)
 diffusion = get_diffusion(crust, model)
 
-num_voxels = diffusion.chunks.__len__ * pow(diffusion.chunk_size, 3)
+num_voxels = diffusion.chunks.__len__() * pow(diffusion.chunk_size, 3)
 g = maxflow.Graph[float](num_voxels * 3, num_voxels * 4)
 nodes = {}
 sorted_keys = sorted(diffusion.chunks.keys())
@@ -31,36 +34,38 @@ for key in sorted_keys:
         continue
 
     # Add nodes
-    set_x = (np.array(key) + (-1, 0, 0)) not in sorted_keys
-    set_y = (np.array(key) + (0, -1, 0)) not in sorted_keys
-    set_z = (np.array(key) + (0, 0, -1)) not in sorted_keys
+    set_x = tuple(np.array(key) + (-1, 0, 0)) not in nodes
+    set_y = tuple(np.array(key) + (0, -1, 0)) not in nodes
+    set_z = tuple(np.array(key) + (0, 0, -1)) not in nodes
     shape_x, shape_y, shape_z = chunk.shape[0] + 1, chunk.shape[1] + 1, chunk.shape[2] + 1
     nodes[key] = np.zeros((shape_x, shape_y, shape_z, 3), dtype=int)
     if not set_x:
-        nodes[key][0] = nodes[tuple((np.array(key) + (-1, 0, 0)))][-1]
+        nodes[key][0] = nodes[tuple(np.array(key) + (-1, 0, 0))][-1]
     else:
         nodes[key][0] = np.array(g.add_nodes(shape_y * shape_z * 3)).reshape(
-            (chunk.shape[1], chunk.shape[2], 3))
+            (shape_y, shape_z, 3))
     if not set_y:
-        nodes[key][:][0] = nodes[tuple((np.array(key) + (0, -1, 0)))][:][-1]
+        nodes[key][:, 0] = nodes[tuple(np.array(key) + (0, -1, 0))][:][-1]
     else:
-        nodes[key][:][0] = np.array(g.add_nodes(shape_x * shape_z * 3)).reshape(
-            (chunk.shape[0], chunk.shape[2], 3))
+        nodes[key][:, 0] = np.array(g.add_nodes(shape_x * shape_z * 3)).reshape(
+            (shape_x, shape_z, 3))
     if not set_z:
-        nodes[key][:][:][0] = nodes[tuple((np.array(key) + (0, 0, -1)))][:][:][-1]
+        nodes[key][:, :, 0] = nodes[tuple(np.array(key) + (0, 0, -1))][:][:][-1]
     else:
-        nodes[key][:][:][0] = np.array(g.add_nodes(shape_x * shape_y * 3)).reshape(
-            (chunk.shape[0], chunk.shape[1], 3))
-    nodes[key][1:][1:][1:] = np.array(
+        nodes[key][:, :, 0] = np.array(g.add_nodes(shape_x * shape_y * 3)).reshape(
+            (shape_x, shape_y, 3))
+    nodes[key][1:, 1:, 1:] = np.array(
         g.add_nodes((shape_x - 1) * (shape_y - 1) * (shape_z - 1) * 3)).reshape(
         (shape_x - 1, shape_y - 1, shape_z - 1, 3))
 
     # Add edges
     capacities = chunk.to_array()
-    for i in enumerate(nodes[key])[:-1]:
-        for j in enumerate(nodes[key][i])[:-1]:
-            for k in enumerate(nodes[key][j])[:-1]:
+    for i, i_ in enumerate(nodes[key][:-1]):
+        for j, j_ in enumerate(nodes[key][i, :-1]):
+            for k, k_ in enumerate(nodes[key][j, :-1]):
                 cap = capacities[i][j][k]
+                if cap == 1.0:
+                    continue
                 n = nodes[key][i][j][k]
                 g.add_edge(n[0], n[1], cap, cap)
                 g.add_edge(n[0], n[2], cap, cap)
@@ -81,19 +86,28 @@ for key in sorted_keys:
 g.maxflow()
 
 surface = {}
-
-for key, n in nodes:
+for key in nodes.keys():
+    n = nodes[key]
     surface[key] = []
-    for i in enumerate(n)[:-1]:
-        for j in enumerate(n[i])[:-1]:
-            for k in enumerate(n[j])[:-1]:
-                node_ids = np.array(list(n[i][j][k]) + n[i + 1][j][k][1] + n[i][j + 1][k][2] + n[i][j][k + 1][0])
+    for i, i_ in enumerate(n[:-1]):
+        for j, j_ in enumerate(n[i, :-1]):
+            for k, k_ in enumerate(n[j, :-1]):
+                node_ids = np.array(list(n[i][j][k]) + [n[i + 1][j][k][1]] + [n[i][j + 1][k][2]] + [n[i][j][k + 1][0]])
                 segs = g.get_grid_segments(node_ids)
                 if not segs.all() and segs.any():
                     surface[key].append((i, j, k))
 
 complete_surface = []
-for key, s in surface:
-    offset = key*diffusion.chunk_size
+for key in surface.keys():
+    if not surface[key]:
+        continue
+    offset = np.array(key)*diffusion.chunk_size
     complete_surface.append(np.array(surface[key]) + offset)
 complete_surface = np.array(complete_surface)
+
+ren = VoxelRender()
+fig = ren.make_figure()
+if model is not None:
+    fig.add_trace(CloudRender().make_scatter(complete_surface, marker=dict(size=0.45), mode="text+markers", name="Model"))
+fig.update_layout(showlegend=True)
+fig.show()
