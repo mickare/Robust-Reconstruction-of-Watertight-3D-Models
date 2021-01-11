4608e90c9abbcc93a64dfcc48180aa55643b02b3
mickare <info@mickare.de>
2021-01-11 17:35:24 +0100

    replace old code

--- playground.py
+++ playground.py
@@ -1,257 +1,70 @@
-from typing import Optional, Tuple, Dict, List
+from typing import Tuple
 
+import numba as nb
 import numpy as np
-import tqdm
-from scipy import signal
-from scipy.sparse import dok_matrix
+from scipy import ndimage
 
-from data.chunks import ChunkGrid, Chunk, ChunkFace
-from filters.dilate import dilate
-from filters.fill import flood_fill_at
-from mathlib import Vec3i, Vec3f
-from model.model_pts import FixedPtsModels
-from render_voxel import VoxelRender
+from mathlib import Vec3i
+
+# @nb.njit(parallel=True)
+# def set_array(arr: np.ndarray, pos: np.ndarray, values: np.ndarray):
+#     assert len(pos) == len(values)
+#     for i in range(len(pos)):
+#         arr[pos[i][0], pos[i][1], pos[i][2]] = values[i]
 from utils import timed
 
-CHUNKSIZE = 8
 
-# =====================================================================
-# Model Loading
-# =====================================================================
-print("Loading model")
-
-
-def scale_model(model: np.ndarray, resolution=64) -> Tuple[np.ndarray, Vec3f, float]:
-    assert model.ndim == 2 and model.shape[1] == 3
-
-    model_min, model_max = np.min(model, axis=0), np.max(model, axis=0)
-    model_delta_max = np.max(model_max - model_min)
-    scale_factor = resolution / model_delta_max
-    scaled = (model - model_min) * scale_factor
-
-    return scaled, model_min, scale_factor
+@nb.njit(parallel=True)
+def set_array_1d(arr: np.ndarray, pos: np.ndarray, values: np.ndarray):
+    for i in nb.prange(len(pos)):
+        arr[pos[i][0], pos[i][1], pos[i][2]] = values[i]
 
 
-resolution = 64
-
-with timed("\tTime: "):
-    data = FixedPtsModels.bunny()
-    data_pts, data_offset, data_scale = scale_model(data, resolution=resolution)
-    model: ChunkGrid[np.bool8] = ChunkGrid(CHUNKSIZE, dtype=np.bool8, fill_value=np.bool8(False))
-    model[data_pts] = True
-    model.pad_chunks(2)
-    model.cleanup()
-
-# =====================================================================
-# Crust Dilation
-# =====================================================================
-print("Dilation")
+@nb.stencil(neighborhood=((-1, 1), (-1, 1), (-1, 1)))
+def kernel3(a):
+    return 0.14285714285714285 * (a[0, 0, 0]
+                                  + a[-1, 0, 0] + a[0, -1, 0] + a[0, 0, -1]
+                                  + a[1, 0, 0] + a[0, 1, 0] + a[0, 0, 1])
 
 
-def find_empty_point_in_chunk(chunk: Chunk[np.bool8]) -> Optional[Vec3i]:
-    if chunk.is_filled():
-        if chunk.value:
-            return chunk.position_low
-    else:
-        pt = np.argwhere(chunk.to_array())
-        if len(pt) > 0:
-            return pt[0] + chunk.position_low
-    return None
+@nb.njit()
+def correlate_iter(shape: Tuple[int, ...], positions, normals: np.ndarray, iterations: int) -> np.ndarray:
+    src = np.zeros(shape, dtype=np.float64)
+    set_array_1d(src, positions, normals)
+
+    dst = np.zeros(shape, dtype=np.float64)
+    for i in range(iterations):
+        # _correlate(src, dst, kernel)
+        kernel3(src, out=dst)
+        # Swap
+        src_old = src
+        src = dst
+        dst = src_old
+        # Reset
+        set_array_1d(src, positions, normals)
+    return src
 
 
-def find_empty_fill_position(mask: ChunkGrid[np.bool8]) -> Optional[Vec3i]:
-    for i, c in mask.chunks.items():
-        if c.any():
-            return find_empty_point_in_chunk(c)
-    return None
+size = 256
+shape = (size, size, size)
+count = 1000
+positions = np.random.randint(0, size, (count, 3), dtype=np.int64)
+positions[0] = (0, 0, 0)
+positions[1] = (0, 1, 1)
+normals = np.random.random(count).astype(np.float64)
+
+print(positions[:2])
+print(normals[:2])
+
+with timed("a: "):
+    result = correlate_iter(shape, positions, normals, 10)
+    print(result[1, :4, :4])
 
 
-def points_on_chunk_hull(grid: ChunkGrid[np.bool8], count: int = 1) -> Optional[np.ndarray]:
-    if not grid.chunks:
-        return None
-
-    pts_iter = (c.position_low for c in grid.iter_hull() if c.is_filled() and not c.value)
-    pts = []
-    for p, _ in zip(pts_iter, range(count)):
-        pts.append(p)
-    if pts:
-        return np.asarray(pts, dtype=int)
-    else:
-        for c in grid.iter_hull():
-            if c.any():
-                p = find_empty_point_in_chunk(c)
-                if p is not None:
-                    return p
-    return None
+@nb.jit()
+def test():
+    return np.zeros(3), np.ones(6)
 
 
-def plot_voxels(grid: ChunkGrid[np.bool8], components: ChunkGrid[int]):
-    ren = VoxelRender()
-    fig = ren.make_figure()
-    fig.add_trace(ren.grid_voxel(grid, opacity=1.0, name=f"Something"))
-    fig.add_trace(ren.grid_voxel(components == 1, opacity=0.2, name=f"Crust"))
-    fig.add_trace(ren.grid_voxel(components == 2, opacity=0.1, name=f"Hull 2"))
-    fig.update_layout(showlegend=True)
-    fig.show()
-
-
-with timed("\tTime: "):
-    max_count = 4
-    last_count = 0
-    crust: ChunkGrid[np.bool8] = model.copy()
-    dilation_step = 0
-
-    for dilation_step in range(30):
-        components = crust.copy(dtype=np.int8)
-        count = 1
-
-        # Find fill components
-        target_fill = points_on_chunk_hull(crust)
-
-        while target_fill is not None:
-            count += 1
-            if count > max_count:
-                break
-
-            fill_mask = flood_fill_at(target_fill, mask=components == 0)
-            components[fill_mask] = count
-            # Update for next iteration
-            target_fill = find_empty_fill_position(components == 0)
-
-        # plot_voxels(components == 0, components)
-
-        if last_count >= count and count <= 3:
-            break
-        else:
-            last_count = count
-            crust = dilate(crust)
-
-    components.cleanup()
-
-crust_outer = dilate(components == 2) & crust
-crust_inner = dilate(components == 3) & crust
-
-print("\tSteps: ", dilation_step)
-
-# =====================================================================
-# Distance Diffusion
-# =====================================================================
-print("Diffusion")
-
-
-def diffuse(model: ChunkGrid[bool], repeat=1):
-    kernel = np.zeros((3, 3, 3), dtype=float)
-    kernel[1] = 1
-    kernel[:, 1] = 1
-    kernel[:, :, 1] = 1
-    kernel /= np.sum(kernel)
-
-    result = model.copy(dtype=float)
-
-    for r in range(repeat):
-        tmp = model.copy(empty=True)
-        for chunk in result.chunks:
-            conv = signal.convolve(chunk.padding(result, 1), kernel, mode='valid', method='direct')
-            conv[model.ensure_chunk_at_index(chunk.index).to_array()] = 1.0
-            tmp.ensure_chunk_at_index(chunk.index).set_array(conv)
-        result = tmp
-
-    result.cleanup()
-    return result
-
-
-with timed("\tTime: "):
-    diff = diffuse(model, repeat=dilation_step)
-
-# =====================================================================
-# Distance Diffusion
-# =====================================================================
-print("MinCut")
-
-# weights = ChunkGrid(diff.chunk_size, dtype=float, fill_value=0)
-weights = diff ** 4 + 1e-05
-weights[~crust] = 0
-weights.cleanup(remove=True)
-
-
-def get_node(pos: Vec3i, face: ChunkFace) -> Tuple[Vec3i, ChunkFace]:
-    """Basically forces to have only positive-direction faces"""
-    if face % 2 == 0:
-        return pos, face
-    else:
-        return tuple(np.add(pos, face.direction, dtype=int)), face.flip()
-
-
-voxels = {tuple(p): w for p, w in weights.items(mask=crust)}
-nodes = list(set(get_node(p, f) for p in voxels.keys() for f in ChunkFace))
-nodes_index = {f: n for n, f in enumerate(nodes)}
-
-nodes_count = len(nodes)
-dok = dok_matrix((nodes_count + 2, nodes_count + 2), dtype=np.float32)
-
-for vPos, w in tqdm.tqdm(voxels.items(), total=len(voxels), desc="Linking Edges"):
-    for f in ChunkFace:  # type: ChunkFace
-        fNode = get_node(vPos, f)
-        fIndex = nodes_index[fNode]
-        for o in f.orthogonal():
-            oNode = get_node(vPos, o)
-            oIndex = nodes_index[oNode]
-            dok[fIndex, oIndex] = w
-            dok[oIndex, fIndex] = w
-
-# Source
-for vPos in tqdm.tqdm(list(crust_outer.where()), desc="Linking Source"):
-    assert crust.get_value(vPos)
-    for f in ChunkFace:  # type: ChunkFace
-        fNode = get_node(tuple(vPos), f)
-        fIndex = nodes_index[fNode]
-        dok[nodes_count, fIndex] = nodes_count
-        dok[fIndex, nodes_count] = nodes_count
-
-# Sink
-for vPos in tqdm.tqdm(list(crust_inner.where()), desc="Linking Sink"):
-    assert crust.get_value(vPos)
-    for f in ChunkFace:  # type: ChunkFace
-        fNode = get_node(tuple(vPos), f)
-        fIndex = nodes_index[fNode]
-        dok[fIndex, nodes_count + 1] = nodes_count
-        dok[nodes_count + 1, fIndex] = nodes_count
-
-import scipy.sparse.csgraph as csg
-
-graph = (dok.tocsr() * 10 ** 5).astype(int)
-res = csg.maximum_flow(graph, nodes_count, nodes_count + 1)
-
-# =====================================================================
-# Render
-# =====================================================================
-print("Render")
-
-with timed("\tTime: "):
-    ren = VoxelRender()
-    fig = ren.make_figure()
-    # fig.add_trace(ren.grid_voxel(model, opacity=0.1, name='Model'))
-    # fig.add_trace(ren.grid_voxel(crust, opacity=0.1, name='Crust'))
-    fig.add_trace(ren.grid_voxel(components == 1, opacity=0.1, name='Crust'))
-    fig.add_trace(ren.grid_voxel(components == 3, opacity=0.1, name='Crust'))
-    fig.add_trace(ren.grid_voxel(components == 4, opacity=0.1, name='Crust'))
-
-    # scat_kwargs = dict(
-    #     marker=dict(
-    #         size=1.0,
-    #         colorscale=[[0.0, 'rgb(0,0,0)'], [1.0, 'rgb(255,255,255)']],
-    #         cmin=0.0,
-    #         cmax=1.0
-    #     ),
-    #     mode="markers"
-    # )
-    #
-    # fig.add_trace(CloudRender().make_value_scatter(diff, mask=crust,
-    #                                                name="Diffusion", **scat_kwargs))
-
-    # fig.add_trace(VoxelRender().grid_voxel(cut, opacity=1.0))
-    #
-    # fig.add_trace(VoxelRender().grid_voxel(inner, opacity=0.1))
-    # fig.add_trace(VoxelRender().grid_voxel(outer, opacity=0.1))
-
-    fig.show()
+a, b = test()
+print(a, b)
